7edf4d9b72606c8c97db985bd89a7b3a21c958f2
acelero-webdev <webdev@acelero.net>
2024-03-29 17:01:29 -0500

    create a Header component and use it in our layout file so that it appears on every page by default

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,11 +3,6 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <nav>
-        <Link href='/performance'>Performance</Link>
-        <Link href='/reliability'>Reliability</Link>
-        <Link href='/scale'>Scale</Link>
-      </nav>
       Home Page
     </div>
   );
