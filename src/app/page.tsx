4535733260d583b5d640d0d381c93e59d2943fde
acelero-webdev <webdev@acelero.net>
2024-03-29 16:47:07 -0500

    add a simple nav using the Link component from next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
-
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <div>Home Page</div>
+    <div>
+      <nav>
+        <Link href='/performance'>Performance</Link>
+        <Link href='/reliability'>Reliability</Link>
+        <Link href='/scale'>Scale</Link>
+      </nav>
+      Home Page
+    </div>
   );
 }
